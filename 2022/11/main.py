c09028dbaadd00687d280b5a3d9579b6a52813ae
Marcos Pasa <marcospasa130@gmail.com>
2023-01-08 15:13:01 -0300

    Part 2 solved

--- 2022/11/main.py
+++ 2022/11/main.py
@@ -1,8 +1,36 @@
+'''
+Reading the data: First is created a monkey object, than the same is put into a monkey list object. 
+This is done for all monkeys in the input.
+
+A monkey object has a method to process all itens that it is holding in the moment, therefore for each
+round I just need to iterate over the monkeys and call this method.
+
+Each part has it's process item method, because the second part it's a little more complex.
+
+Part 1: 
+    This part was solved very straightforward, just fallowing the rules the puzzle stated.
+
+Part 2:
+    Every monkey has a test to perform when deciding for which monkey to pass the item. This
+    test consists checking the divisibility of the item worry level by some number, which I will 
+    call `test_num`.
+    
+    Instead of keeping items worry levels, for each item is calculated it's remainder by every `test_num`.
+    Then this remainders are used when processing the items, instead the items worry levels. The only difference 
+    is that it's needed to recalculate the remainder after the operation is applied to the remainder.
+'''
+
 import AdventOfCode.aoc as aoc
 
 class Monkey:
-    def __init__(self, items: list[int], operation, test_num: int, test_true_id: int, test_false_id: int, monkey_list: list) -> None:
+    def __init__(self, id, items: list[int], operation, test_num: int, test_true_id: int, test_false_id: int, monkey_list: list) -> None:
+        self.id = id
+        
         self.items = items
+        
+        # Used for part 2
+        self.items_remainders = []
+
         self.operation = operation
         
         self.test_num = test_num
@@ -11,11 +39,10 @@ class Monkey:
         
         self.monkey_list = monkey_list
 
-        self.num_process_itens = 0
+        self.num_process_items = 0
 
-
-    def process_itens(self):
-        self.num_process_itens += len(self.items)
+    def process_items(self):
+        self.num_process_items += len(self.items)
         for item in self.items:
             item = self.operation(item)
             item = int(item / 3)
@@ -26,6 +53,54 @@ class Monkey:
                 self.monkey_list[self.test_false_id].items.append(item)
 
         self.items = []
+
+    def process_items_part_2(self):
+        self.num_process_items += len(self.items_remainders)
+        for item in self.items_remainders:
+            new_item = []
+            for remainder, test_num in zip(item, test_num_list):
+                new_item.append(self.operation(remainder) % test_num)
+
+            if new_item[self.id] == 0:
+                self.monkey_list[self.test_true_id].items_remainders.append(new_item)
+            else:
+                self.monkey_list[self.test_false_id].items_remainders.append(new_item)
+
+        self.items_remainders = []
+
+class MonkeyList:
+    def __init__(self) -> None:
+        self.monkeys = []
+        self.test_num_list = []
+
+    def add_monkey(self, monkey: Monkey):
+        self.monkeys.append(monkey)
+    
+    def reset_num_process_items(self):
+        monkey: Monkey
+        for monkey in self.monkeys:
+            monkey.num_process_items = 0
+
+    def create_items_remainders(self):
+        monkey: Monkey
+        for monkey in self.monkeys:
+            items_remainders = []
+            for item in monkey.items:
+                remainders = [(item % test_num) for test_num in test_num_list]
+                items_remainders.append(remainders)
+            
+            monkey.items_remainders = items_remainders
+        
+    @property
+    def monkey_business(self):
+        monkeys_process_itens = []
+        monkey: Monkey
+        for monkey in self.monkeys:
+            monkeys_process_itens.append(monkey.num_process_items)
+
+        monkeys_process_itens.sort()
+
+        return monkeys_process_itens[-1] * monkeys_process_itens[-2]
 
 # Read Input ###
 input_path = "input.txt"
@@ -63,7 +138,8 @@ def get_test_info(string: list[str]):
 
 count = 1
 monkey_id = 0
-monkey_list = []
+monkey_list = MonkeyList()
+test_num_list = []
 while count < len(data_temp):
     items = data_temp[count].split(":")[-1].split(",")
     items = [int(i.strip()) for i in items]
@@ -71,34 +147,39 @@ while count < len(data_temp):
     operation = get_operation(data_temp[count + 1])
 
     test_num, test_true_id, test_false_id = get_test_info(data_temp[count + 2: count + 5])
+    test_num_list.append(test_num)
 
-    monkey = Monkey(items, operation, test_num, test_true_id, test_false_id, monkey_list)
-    monkey_list.append(monkey)
+    monkey = Monkey(monkey_id, items, operation, test_num, test_true_id, test_false_id, monkey_list.monkeys)
+    monkey_list.add_monkey(monkey)
 
     monkey_id += 1
     count += 7
 ###
 
+monkey_list.test_num_list = test_num_list
+monkey_list.create_items_remainders()
 
 ### Part 1 ###
 print("="*5, "Part 1", "="*5)
 
 monkey: Monkey
 for round_id in range(20):
-    for monkey in monkey_list:
-        monkey.process_itens()
+    for monkey in monkey_list.monkeys:
+        monkey.process_items()
 
-monkeys_process_itens = []
-for monkey in monkey_list:
-    monkeys_process_itens.append(monkey.num_process_itens)
-
-monkeys_process_itens.sort()
-print("Monkey business:", monkeys_process_itens[-1] *monkeys_process_itens[-2])
+print("Monkey business:", monkey_list.monkey_business)
 ###
 
 print()
 
 ### Part 2 ###
 print("="*5, "Part 2", "="*5)
+monkey_list.reset_num_process_items()
 
+monkey: Monkey
+for round_id in range(10000):
+    for monkey in monkey_list.monkeys:
+        monkey.process_items_part_2()
+
+print("Monkey business:", monkey_list.monkey_business)
 ###
